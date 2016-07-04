2c1c3d1d552c0bda969521d60c7a790ca9128729
DavidGayo <crowin@hotmail.com>
2016-07-04 17:12:35 -0500

    quedo igual 04/06/2016

--- application/models/Usuarios_model.php
+++ application/models/Usuarios_model.php
@@ -5,51 +5,48 @@ class Usuarios_model extends CI_Model
     { 
         parent:: __construct(); 
     }
+
     public function getUsuarios()
     {
     	$query=$this->db
     	->select("id,nombre,usuario,contrasena")
-    	->from("Usuarios")
+    	->from("usuarios")
         ->order_by("id","desc")
     	->get();
     	return $query->result();
-    }        
+    }
+
     public function insertar_usuario($datos=array())
     {
-        $this->db->insert("Usuarios",$datos);
+        $this->db->insert("usuarios",$datos);
         return true;
     }
+
     public function getUsuarioId($id)
     {
         $where=array("id"=>$id);
         $query=$this->db
         ->select("id,nombre,usuario,contrasena")
-        ->from("Usuarios")
+        ->from("usuarios")
         ->where($where)
         ->order_by("id","desc")
         ->get();
         return $query->row();
     }
+
     public function modificar_usuario($datos=array(),$id)
     {
         $this->db->where('id',$id);
         $this->db->update('usuarios',$datos);
         return true;
     }
+
     public function eliminar($id=null)
     {
         $this->db->delete('usuarios',array('id' => $id));
         return true;
     } 
-    /*
-    public function getUsuariosConEquipo()
-    {
-        $query=$this->db
-        ->select("u.id as id_usuario,u.nombre,u.usuario,u.contresena,e.dyndns,e.usuario,e.contrasena")
-        ->from("usuarios as u")
-        ->join("equipo_usuario as e","u.id = e.id_usuario","inner")
-        ->get();
-        return $query->result();
-    }*/
+    
+
 
 }
