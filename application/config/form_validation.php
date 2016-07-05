ba39983aa4997ce307536d6ba40d624585e9ee36
DavidGayo <crowin@hotmail.com>
2016-07-05 14:58:02 -0500

    validador de formulario de equipo 05/07/2016

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -8,8 +8,14 @@ $config=array
         => array(
         	array('field' => 'nom',   'label' => 'Nombre',   'rules' => 'required|is_string|trim|xss_clean'),
         	array('field' => 'usua',   'label' => 'Usuario',   'rules' => 'required|is_string|trim|xss_clean'),
-        	array('field' => 'pass',   'label' => 'Contrasena',   'rules' => 'required|is_string|trim|xss_clean')
+        	array('field' => 'pass',   'label' => 'Contraseña',   'rules' => 'required|is_string|trim|xss_clean')
         	),
+    'warriors/nuevo'
+        => array(
+            array('field' => 'dyn', 'label' => 'DynDNS', 'rules' => 'required|trim|xss_clean'),
+            array('fiel' => 'usua', 'label' => 'Usuario', 'rules' => 'required|trim|xss_clean'),
+            array('fiel' => 'pass', 'label' => 'Contraseña', 'rules' => 'required|trim|xss_clean')
+            ),
 
 
 	);
