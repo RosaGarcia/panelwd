6d22b9b1638bba826e56aa1552383dde4e5028b3
DavidGayo <crowin@hotmail.com>
2016-07-05 14:59:49 -0500

    modelo de equipos 05/07/2016

---
+++ application/models/Equipos_model.php
@@ -0,0 +1,52 @@
+<?php 
+class Equipos_model extends CI_Model
+{
+    function  __construct() 
+    { 
+        parent:: __construct(); 
+    }
+
+    public function getEquipos()
+    {
+    	$query=$this->db
+    	->select("id,dyndns")
+    	->from("equipos")
+        ->order_by("id","desc")
+    	->get();
+    	return $query->result();
+    }
+
+    public function insertarEquipos($datos=array())
+    {
+        $this->db->insert("equipos",$datos);
+        return true;
+    }
+
+    public function getEditEquipos($id)
+    {
+        $where=array("id"=>$id);
+        $query=$this->db
+        ->select("dyndns,usuario,contrasena")
+        ->from("equipos")
+        ->where($where)
+        ->order_by("id","desc")
+        ->get();
+        return $query->row();
+    }
+
+    public function editEquipos($datos=array(),$id)
+    {
+        $this->db->where('id',$id);
+        $this->db->update('equipos',$datos);
+        return true;
+    }
+
+    public function eliminar($id=null)
+    {
+        $this->db->delete('equipos',array('id' => $id));
+        return true;
+    } 
+    
+
+
+}
