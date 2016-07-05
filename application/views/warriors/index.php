7b080b81b3397d3bdb622803190c5bc74e25f873
DavidGayo <crowin@hotmail.com>
2016-07-05 14:59:26 -0500

    vista index de equipo 05/07/2016

--- application/views/warriors/index.php
+++ application/views/warriors/index.php
@@ -1,72 +1,68 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Warriors</title>	
-</head>
-<section class="panel">
-	<div class="panel-body">
-		<div class="top-stats-panel">
-			<body>
-				<h1>Home</h1>
-				<!--<p>
-					<a href="<?php echo base_url()?>warriors/new">Nuevo Equipo</a>
-				</p>-->
-				<?php 
-				echo form_open_multipart(null);
-				?>
-				<div class="row">
-					<div class="col-xs-12 col-sm-12 col-md-12">
-						<section class="panel">
-						    <header class="panel-heading">
-						     	Info equipos			     	
-						        <span class="tools pull-right">
-						        	<a href="javascript:;"" class="fa fa-chevron-down"></a>
-						        	<a href="javascript:;"" class="fa fa-cog"></a>
-						        	<a href="javascript:;"" class="fa fa-times"></a>
-						        </span>	
-						    </header>
-						    <div class="panel-body">
-							    <table class="table table-hover general-table">	
-							        <thead>
-							        	<tr>
-							        		<th>DynDNS</th>
-							        		<th>Graficas</th>
-							        		<th>Acciones</th>
-							        	</tr>
-							        </thead>
-							        <tbody>
-							        	<tr>
-							        		<td>Ola</td>
-							        		<td>
-							        		    <div class="col-md-3">
-												    <div class="top-stats-panel">
-												        <div class="gauge-canvas">
-												            <h4 class="widget-h">CPU</h4>
-												            <canvas width=160 height=100 id="gauge"></canvas>
-												        </div>
-												        <ul class="gauge-meta clearfix">
-												            <li id="gauge-textfield" class="pull-left gauge-value"></li>
-												            <li class="pull-right gauge-title">Safe</li>
-												        </ul>
-												    </div>
-												</div>
-							        		</td>
-							        		<td>
-							        		    <a href="<?php echo base_url()?>warriors/show" class="btn btn-round btn-primary">VER</a>	    	
-							        		</td>
-							        	</tr>
-							        </tbody>
-							    </table>
-						    </div>			
-						</section>
+<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+
+	<section class="panel">
+		<div class="panel-body">
+			<div class="top-stats-panel">
+					<h1>Home</h1>
+					<div class="row">
+						<div class="col-xs-12 col-sm-12 col-md-12">
+							<section class="panel">
+							    <header class="panel-heading">
+							     	Info equipos
+							    </header>
+							    <div class="panel-body">
+								    <table class="table table-hover general-table">	
+								        <thead>
+								        	<tr>
+								        		<th>DynDNS</th>
+								        		<th>Graficas</th>
+								        		<th>Acciones</th>
+								        	</tr>
+								        </thead>
+								        <tbody>
+								       	<?php 
+									    	foreach ($datos as $dato) 
+									    	{
+										    	?>  
+												<tr>
+													<td><a href=""><?php echo $dato->dyndns ?></a></td>
+													<td><span id="cpu<?php echo $dato->id ?>"></span>
+													</td>
+													<td>
+														<a href="<?php echo base_url() ?>warriors/show/<?php echo $dato->id ?>" class="btn btn-round btn-primary">VER</a>
+													</td>
+												</tr>			
+										    <?php 
+											}
+										?>
+								        </tbody>
+								    </table>
+							    </div>			
+							</section>
+						</div>
 					</div>
-				</div>
-				<?php echo form_close();?>
-			</body>
+			</div>
 		</div>
-	</div>
-</section>			
-</html>
+	</section>
+
+	<script type="text/javascript">
+			/*var servidor = <?php echo json_encode($datos);?>;
+			var ip = servidor[1]['dyndns'];
+			alert("http://"+ip+"/prueba.php?jsoncallback");*/
+		function peticion(){
+			var servidor = <?php echo json_encode($datos);?>;
+				for(var i = 0 ; i<=servidor.length; i ++){
+					$.getJSON("http://"+servidor[i]['dyndns']+"/prueba.php?jsoncallback=?").done(function(datos,i){
+
+					//$("#saludo").append("<p>" + datos.infoInterlan['ipaddr'] + "</p>");
+					document.getElementById("cpu"+servidor[1]['id'] ).innerHTML = "<p>" + datos.cpu + "</p>";
+					document.getElementById("mbuf").innerHTML = "<p>" + datos.mbuf + "</p>";
+					})
+				}
+		}
+
+		setInterval('peticion()',10000);
+
+	</script>			
+
+
