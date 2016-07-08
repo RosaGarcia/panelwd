e3b008ccc9fa70c8d7514362578100403d1de8af
DavidGayo <crowin@hotmail.com>
2016-07-08 14:02:59 -0500

    login correcto 08/07/2016

--- application/controllers/Verifylogin.php
+++ application/controllers/Verifylogin.php
@@ -1,66 +1,41 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class VerifyLogin extends CI_Controller {
+class VerifyLogin extends CI_Controller 
+{
  
- function __construct()
- {
-   parent::__construct();
-   $this->load->model('Usuarios_model','',TRUE);
- }
- 
- function index()
- {
-   //This method will have the credentials validation
-   $this->load->library('form_validation');
-
-
-   $this->form_validation->set_rules('username', 'Username', 'trim|required');
-   $this->form_validation->set_rules('password', 'Password', 'trim|required|callback_check_database');
- 
-   if($this->form_validation->run() == FALSE)
+   function __construct()
    {
-     //Field validation failed.  User redirected to login page
-     $this->load->helper(array('form'));
-     $this->load->view('login/login_view');
+     parent::__construct();
+     $this->load->model('Usuarios_model','',TRUE);
    }
-   else
-   {
-     //Go to private area
-     //$session_data = $this->session->userdata('logged_in');
-     //$data['usuario'] = $session_data['usuario'];
-     $datos=$this->Equipos_model->getEquipos();
-     redirect(base_url().'',compact("datos"));
-     //$this->load->view('warriors/index',compact("datos"));   
-   }
-
- }
- 
- function check_database($password)
- {
-   //Field validation succeeded.  Validate against database
-   $username = $this->input->post('username');
    
-   //query the database
-   $result = $this->Usuarios_model->login($username, $password);
- 
-   if($result)
+   function index()
    {
-     $sess_array = array();
-     foreach($result as $row)
+     //Field validation succeeded.  Validate against database
+     $username = $this->input->post('username');
+     $password = $this->input->post('password');
+     
+     //query the database
+     $result = $this->Usuarios_model->login($username, $password);
+   
+     if($result)
      {
+       $sess_array = array();
+       foreach($result as $row)
+       {
 
-       $sess_array = array(
-         'id' => $row->id,
-         'usuario' => $row->usuario
-       );
-       $this->session->set_userdata('logged_in', $sess_array);
+         $sess_array = array(
+           'id' => $row->id,
+           'usuario' => $row->usuario
+         );
+         $this->session->set_userdata('logged_in', $sess_array);
+       }
+       $datos=$this->Equipos_model->getEquipos();
+       redirect(base_url().'',compact("datos"));
      }
-     return TRUE;
+     else
+     {
+        $this->session->set_flashdata('ControllerMessage', 'Usuario o contraseña incorrecta');
+        $this->load->view('login/login_view');
+     }
    }
-   else
-   {
-     $this->form_validation->set_message('check_database', 'Invalid username or password');
-     return false;
-   }
- }
 }
-?>
