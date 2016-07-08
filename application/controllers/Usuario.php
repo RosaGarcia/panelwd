b7bbcde937c89f822650b2f9b6109f1459abae55
DavidGayo <crowin@hotmail.com>
2016-07-08 14:02:33 -0500

    secion en cada vista 08/07/2016

--- application/controllers/Usuario.php
+++ application/controllers/Usuario.php
@@ -11,76 +11,105 @@ class Usuario extends CI_Controller
 
 	public function index()
 	{	
-		$datos=$this->Usuarios_model->getUsuarios();
-		$this->layout->view('index',compact("datos"));
+        if($this->session->userdata('logged_in'))
+        {
+    		$datos=$this->Usuarios_model->getUsuarios();
+    		$this->layout->view('index',compact("datos"));
+        }
+        else
+        {
+            $this->load->helper(array('form'));
+            $this->load->view('login/login_view');
+
+        }
 	}
 
     public function nuevo()
     {
-        if($this->input->post())
+        if($this->session->userdata('logged_in'))
         {
-            if($this->form_validation->run("usuario/nuevo") == false)
+            if($this->input->post())
             {
-              
-                $datos=array
-                (
-                    'nombre' => $this->input->post("nom",true),
-                    'usuario' => $this->input->post("usua",true),
-                    'contrasena' => sha1($this->input->post("pass",true))
-                );
-                $guardar=$this->Usuarios_model->insertar_usuario($datos);
-                if($guardar)
+                if($this->form_validation->run("usuario/nuevo"))
                 {
-                    $this->session->set_flashdata('ControllerMessage', 'Se ha agragado el ususario exitosamente');
-                    redirect(base_url().'usuario/index', 301);
-                }
-                else
-                {
-                    $this->session->set_flashdata('ControllerMessage', 'Se ha producido un error. Inténtelo de nuevo.');
-                    redirect(base_url().'usuario/new',  301);
+                  
+                    $datos=array
+                    (
+                        'nombre' => $this->input->post("nom",true),
+                        'usuario' => $this->input->post("usua",true),
+                        'contrasena' => md5($this->input->post("pass",true))
+                    );
+                    $guardar=$this->Usuarios_model->insertar_usuario($datos);
+                    if($guardar)
+                    {
+                        $this->session->set_flashdata('ControllerMessage', 'Se ha agragado el ususario exitosamente');
+                        redirect(base_url().'usuario/index', 301);
+                    }
+                    else
+                    {
+                        $this->session->set_flashdata('ControllerMessage', 'Se ha producido un error. Inténtelo de nuevo.');
+                        redirect(base_url().'usuario/new',  301);
+                    }
                 }
             }
-        }
 
-        $this->layout->view('new');
+            $this->layout->view('new');
+        }
+        else
+        {
+            $this->load->helper(array('form'));
+            $this->load->view('login/login_view');
+
+        }
     }
 
     public function edit($id=null)
     {
-        if($this->input->post())
+        if($this->session->userdata('logged_in'))
         {
-            if($this->form_validation->run("usuario/nuevo") == false)
-            { 
-                $datos=array
-                (
-                    'nombre'=> $this->input->post("nom",true),
-                    'usuario'=> $this->input->post("usua",true),
-                    'contrasena'=> sha1($this->input->post('pas',true))   
-                    );
-                $guardar=$this->Usuarios_model->modificar_usuario($datos,$id);
-                if($guardar)
-                {
-                    $this->session->set_flashdata('ControllerMessage', 'Se ha editado el ususario exitosamente');
-                    redirect(base_url().'usuario/index', 301);
-                }
-                else
-                {
-                    $this->session->set_flashdata('ControllerMessage', 'Se ha producido un error. Inténtelo de nuevo.');
-                    redirect(base_url().'usuario/edit/'.$id,  301);
+            if($this->input->post())
+            {
+                if($this->form_validation->run("usuario/nuevo"))
+                { 
+                    $datos=array
+                    (
+                        'nombre'=> $this->input->post("nom",true),
+                        'usuario'=> $this->input->post("usua",true),
+                        'contrasena'=> sha1($this->input->post('pas',true))   
+                        );
+                    $guardar=$this->Usuarios_model->modificar_usuario($datos,$id);
+                    if($guardar)
+                    {
+                        $this->session->set_flashdata('ControllerMessage', 'Se ha editado el ususario exitosamente');
+                        redirect(base_url().'usuario/index', 301);
+                    }
+                    else
+                    {
+                        $this->session->set_flashdata('ControllerMessage', 'Se ha producido un error. Inténtelo de nuevo.');
+                        redirect(base_url().'usuario/edit/'.$id,  301);
+                    }
                 }
             }
+                    $datos=$this->Usuarios_model->getUsuarioId($id);
+                    if(sizeof($datos)==0)
+                    {
+                        show_404();
+                    } 
+                    $this->layout->view('edit',compact("datos"));
         }
-                $datos=$this->Usuarios_model->getUsuarioId($id);
-                if(sizeof($datos)==0)
-                {
-                    show_404();
-                } 
-                $this->layout->view('edit',compact("datos"));
+        else
+        {
+            $this->load->helper(array('form'));
+            $this->load->view('login/login_view');
+
+        }
     }
 
     public function delete($id=null)
     {
-        $guardar=$this->Usuarios_model->eliminar($id);
+        if($this->session->userdata('logged_in'))
+        {
+            $guardar=$this->Usuarios_model->eliminar($id);
             if($guardar)
             {
                 $this->session->set_flashdata('ControllerMessage', 'Se ha eliminado el ususario exitosamente');
@@ -91,6 +120,14 @@ class Usuario extends CI_Controller
                 $this->session->set_flashdata('ControllerMessage', 'Se ha producido un error. Inténtelo de nuevo.');
                 redirect(base_url().'usuario/index',  301);
             }
+        }
+        else
+        {
+            $this->load->helper(array('form'));
+            $this->load->view('login/login_view');
+
+        }
     }
+
 }
 
